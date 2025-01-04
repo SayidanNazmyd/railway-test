a3841d4746ba009a78e1c4769b90c8dae495bcbf
SayidanNazmyd <nazmydsayidan@gmail.com>
2025-01-04 13:25:17 +0700

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,29 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET } = process.env
 
 module.exports = function (req, res, next) {
-  const token = req.header("Authorization");
+  // Get token from header
+  const token = req.header('Authorization');
 
+  // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
+  // Verify token
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = { id: decoded.id }; // Ambil ID dari decoded token
-    next();
+    jwt.verify(token, JWT_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
   } catch (err) {
-    console.error("something wrong with auth middleware");
-    res.status(500).json({ msg: "Server Error" });
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
   }
 };
