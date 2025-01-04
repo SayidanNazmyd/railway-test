8315b0234b76b364462dc742029bf2ea511f4e9a
SayidanNazmyd <nazmydsayidan@gmail.com>
2025-01-04 13:27:44 +0700

    update

--- controllers/todos.js
+++ controllers/todos.js
@@ -13,20 +13,16 @@ exports.getTodos = async(req, res) => {
   }
 }
 
-exports.createTodo = async (req, res) => {
+exports.createTodo= async(req, res) => {   
   try {
-    const todo = await Todo.create({
-      title: req.body.title,
-      user: req.user.id, 
-    });
-
+    const todo = await Todo.create(req.body)
     res.status(200).json({
       message: "success",
-      data: todo,
-    });
-  } catch (err) {
+      data: todo
+    })
+  }catch(err){
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 };
 
