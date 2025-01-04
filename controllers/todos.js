f73747a8ea1746951a7107e4da04e9ec78aac9f6
SayidanNazmyd <nazmydsayidan@gmail.com>
2025-01-04 13:32:24 +0700

    update

--- controllers/todos.js
+++ controllers/todos.js
@@ -13,16 +13,20 @@ exports.getTodos = async(req, res) => {
   }
 }
 
-exports.createTodo= async(req, res) => {   
+exports.createTodo = async (req, res) => {
   try {
-    const todo = await Todo.create(req.body)
+    const todo = await Todo.create({
+      title: req.body.title,
+      user: req.user.id, 
+    });
+
     res.status(200).json({
       message: "success",
-      data: todo
-    })
-  }catch(err){
+      data: todo,
+    });
+  } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,21 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
 
-const { JWT_SECRET } = process.env
+const { JWT_SECRET } = process.env;
 
 module.exports = function (req, res, next) {
-  // Get token from header
-  const token = req.header('Authorization');
+  const token = req.header("Authorization");
 
-  // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
-  // Verify token
   try {
-    jwt.verify(token, JWT_SECRET, (error, decoded) => {
-      if (error) {
-        return res.status(401).json({ msg: 'Token is not valid' });
-      } else {
-        req.user = decoded.user;
-        next();
-      }
-    });
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = { id: decoded.id }; // Ambil ID dari decoded token
+    next();
   } catch (err) {
-    console.error('something wrong with auth middleware');
-    res.status(500).json({ msg: 'Server Error' });
+    console.error("something wrong with auth middleware");
+    res.status(500).json({ msg: "Server Error" });
   }
 };
